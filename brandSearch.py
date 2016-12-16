132c0502e9dd90c917b0085268c2201b606bd8af
Juta <juta.staes@ugent.be>
2016-12-16 17:35:28 +0100

    rolex search

--- brandSearch.py
+++ brandSearch.py
@@ -1,24 +1,23 @@
 #! /bin/python
 from pyspark import SparkContext, SparkConf
 
+def searchBrand(line):
+	if("Rolex" in line):
+		return ("Rolex", 1) 
+	else:
+		return ("No Brand", 1)
 
 conf = SparkConf().setAppName("SoA").setMaster("local")
 sc = SparkContext(conf=conf)
 
 metadata_path = "hdfs:///datasets/amazon-reviews/metadata.json"
+#metadata_path = "file:///home/staes/shuffled_metadata.json"
 text_file = sc.textFile(metadata_path)
 print('ok')
 
 counts = text_file \
-             .map(lambda word: ("a", 1)) \
+             .map(searchBrand) \
              .reduceByKey(lambda a, b: a + b)
 print(counts.collect())
 #.flatMap(lambda line: line.split(" ")) \
-#counts.saveAsTextFile("test.txt")
-
-'''
-counts = text_file.flatMap(lambda line: line.split(" ")) \
-             .map(lambda word: (word, 1)) \
-             .reduceByKey(lambda a, b: a + b)
-counts.saveAsTextFile("/home/staes/test.txt")
-'''
+#counts.saveAsTextFile("file:///home/staes/test.txt")
